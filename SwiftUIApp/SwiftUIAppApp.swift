c183828a20fe60946214f5b1ff5bbcddb73183b4
Eugene Kiselev <zhenya-kiselev-97@mail.ru>
2021-04-17 18:48:06 +0300

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Eugene Kiselev on 17.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
